3b74646c35f12587d1a90cbd0254d7aac9ad5e84
Brenno lima <brenno.marquesb@gmail.com>
2024-05-14 01:10:13 -0300

    setup: changed seed product name

--- database/factories/ProductsFactory.php
+++ database/factories/ProductsFactory.php
@@ -16,9 +16,22 @@ class ProductsFactory extends Factory
      */
     public function definition(): array
     {
+        $names = [
+            'Arroz Tio Jorge',
+            'Feijão Carioca',
+            'Óleo de Soja',
+            'Café da Roça',
+            'Açúcar Cristal',
+            'Farinha de Trigo',
+            'Macarrão Espaguete',
+            'Leite ninha',
+            'Biscoito Recheado',
+            'Sabonete Avon'
+        ];
+
         return [
             'barcode' => $this->faker->unique()->isbn13,
-            'name' => $this->faker->name,
+            'name' => $this->faker->randomElement($names),
             'price' => $this->faker->randomFloat(2, 10, 100),
             'qty_stock' => $this->faker->numberBetween(0, 100),
         ];
